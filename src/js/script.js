06990f64e0db59382d863fe1a945015c5b26cd43
Mateusz Mikolajczyk <mikolajczykmateusz1993@gmail.com>
2020-03-18 18:22:32 +0000

    Add amount widget funcionality

--- src/js/script.js
+++ src/js/script.js
@@ -64,9 +64,10 @@
       thisProduct.renderInMenu();
       //[DONE] Add metod getElements
       thisProduct.getElements();
-      console.log('new Product: ', thisProduct);
+      //console.log('new Product: ', thisProduct);
       thisProduct.initAccordion();
       thisProduct.initOrderForm();
+      thisProduct.initAmountWidget();
       thisProduct.processOrder();
     }
 
@@ -92,25 +93,24 @@
       thisProduct.cartButton = thisProduct.element.querySelector(select.menuProduct.cartButton);
       thisProduct.priceElem = thisProduct.element.querySelector(select.menuProduct.priceElem);
       thisProduct.imageWrapper = thisProduct.element.querySelector(select.menuProduct.imageWrapper);
+      thisProduct.amountWidgetElem = thisProduct.element.querySelector(select.menuProduct.amountWidget);
     }
 
     //[DONE]  metod
     initAccordion() {
-
       const thisProduct = this;
       /* find the clickable trigger (the element that should react to clicking) */
       const accordionTrigger = thisProduct.element.querySelector(select.menuProduct.clickable);
-
       /* START: click event listener to trigger */
       accordionTrigger.addEventListener('click', function () {
         /* prevent default action for event */
         event.preventDefault();
-        console.log('clicked');
+        //console.log('clicked');
         /* toggle active class on element of thisProduct */
         thisProduct.element.classList.toggle('active');
         /* find all active products */
         const allActiveProducts = document.querySelectorAll(select.all.menuProductsActive);
-        console.log('all active products: ', allActiveProducts);
+        //console.log('all active products: ', allActiveProducts);
         /* START LOOP: for each active product */
         for (let activeProduct of allActiveProducts) {
           /* START: if the active product isn't the element of thisProduct */
@@ -127,7 +127,7 @@
     //[DONE] Add initOrderForm method
     initOrderForm() {
       const thisProduct = this;
-      console.log('initOrderForm :', thisProduct);
+      //console.log('initOrderForm :', thisProduct);
 
       thisProduct.form.addEventListener('submit', function (event) {
         event.preventDefault();
@@ -148,34 +148,29 @@
     //[IN PROGRESS] Add processOrder method
     processOrder() {
       const thisProduct = this;
-      console.log('processOrder: ', thisProduct);
+      //console.log('processOrder: ', thisProduct);
       /*[DONE] read all data from the form (using utils.serializeFormToObject) and save it to const formData */
       const formData = utils.serializeFormToObject(thisProduct.form);
-      console.log('formData', formData);
+      //console.log('formData', formData);
       /* set variable price to equal thisProduct.data.price */
       let price = thisProduct.data.price;
-      console.log('Price: ', price);
-
+      //console.log('Price: ', price);
       const images = thisProduct.imageWrapper;
       console.log('List of images : ', images);
-
-
       /* START LOOP: for each paramId in thisProduct.data.params */
-
       for (let paramId in thisProduct.data.params) {
-        console.log('Param id :', paramId);
+        //console.log('Param id :', paramId);
         /* save the element in thisProduct.data.params with key paramId as const param */
         const param = thisProduct.data.params[paramId];
-        console.log('Parameters :', param);
+        //console.log('Parameters :', param);
         /* START LOOP: for each optionId in param.options */
-
         for (let optionId in param.options) {
           /* save the element in param.options with key optionId as const option */
           const option = param.options[optionId];
-          console.log('option :', option);
+          //console.log('option :', option);
           /* START IF: if option is selected and option is not default */
           const optionSelected = formData.hasOwnProperty(paramId) && formData[paramId].indexOf(optionId) > -1;
-          console.log('optionSelected', optionSelected);
+          //console.log('optionSelected', optionSelected);
           if (optionSelected && !option.default) {
             price = price + option.price;
             /* END IF: if option is selected and option is not default */
@@ -185,7 +180,6 @@
             /* deduct price of option from price */
             price = price - option.price;
           }
-
           const selectedImg = thisProduct.imageWrapper.querySelectorAll(`.${paramId}-${optionId}`);
           if(optionSelected){
             for (let img of selectedImg){
@@ -194,26 +188,93 @@
           }else{
             for (let img of selectedImg){
               img.classList.remove(classNames.menuProduct.imageVisible);
-
             }
           }
-
         }
         /* END LOOP: for each optionId in param.options */
-
-
-
       }
       /* END LOOP: for each paramId in thisProduct.data.params */
-
+      /*multiply price by amount*/
+      price *= thisProduct.amountWidget.value;
       /* set the contents of thisProduct.priceElem to be the value of variable price */
       thisProduct.priceElem.innerHTML = price;
+    }
 
+    initAmountWidget(){
+      const thisProduct = this;
+      thisProduct.amountWidget = new AmountWidget(thisProduct.amountWidgetElem)
+
+      thisProduct.amountWidgetElem.addEventListener('updated', function(){
+        thisProduct.processOrder();
+      });
     }
   }
 
-  // [DONE] Add declaration of metod app.initMenu
+  class AmountWidget{
+    constructor(element){
+      const thisWidget = this;
 
+      thisWidget.getElements(element);
+      thisWidget.value = settings.amountWidget.defaultValue;
+      thisWidget.setValue(thisWidget.input.value);
+      thisWidget.initActions();
+      console.log('AmountWidget : ', thisWidget);
+      console.log('constructor arguments : ', element);
+    }
+
+    getElements(element){
+      const thisWidget = this;
+
+      thisWidget.element = element;
+      thisWidget.input = thisWidget.element.querySelector(select.widgets.amount.input);
+      thisWidget.linkDecrease = thisWidget.element.querySelector(select.widgets.amount.linkDecrease);
+      thisWidget.linkIncrease = thisWidget.element.querySelector(select.widgets.amount.linkIncrease);
+      thisWidget.value = settings.amountWidget.defaultValue;
+    }
+
+    setValue(value) {
+      const thisWidget = this;
+      const newValue = parseInt(value);
+
+      /* To do: Add validation*/
+      if (newValue != thisWidget.value
+        && newValue >= settings.amountWidget.defaultMin
+        && newValue <= settings.amountWidget.defaultMax){
+        thisWidget.value = newValue;
+        thisWidget.announce();
+      }
+
+      thisWidget.input.value = thisWidget.value;
+    }
+
+    initActions(){
+      const thisWidget = this;
+      console.log(thisWidget);
+      //Add event listener
+      thisWidget.input.addEventListener('change', function(){
+        thisWidget.setValue(thisWidget.input.value);
+      });
+
+      thisWidget.linkIncrease.addEventListener('click', function(event){
+        event.preventDefault;
+        thisWidget.setValue(thisWidget.value + 1);
+      });
+
+      thisWidget.linkDecrease.addEventListener('click', function(event){
+        event.preventDefault;
+        thisWidget.setValue(thisWidget.value - 1);
+      });
+    }
+    announce() {
+      const thisWidget = this;
+
+      const event = new Event('updated');
+      thisWidget.element.dispatchEvent(event);
+    }
+  }
+
+
+  // [DONE] Add declaration of metod app.initMenu
   const app = {
     initMenu: function () {
       const thisApp = this;
@@ -222,10 +283,8 @@
       for (let productData in thisApp.data.products) {
         new Product(productData, thisApp.data.products[productData]);
       }
-
       const testProduct = new Product();
       console.log('testProduct: ', testProduct);
-
     },
     // [DONE Add metod app.initData
     initData: function () {
@@ -244,8 +303,6 @@
       thisApp.initMenu();
     },
   };
-
-
 
   app.init();
 }
